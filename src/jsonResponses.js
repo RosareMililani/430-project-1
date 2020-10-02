f5e7759171c43093499e6b52294824bc0e7c52f8
RosareMililani <mr7721@rit.edu>
2020-10-02 05:51:49 -0400

    initial commit

---
+++ src/jsonResponses.js
@@ -0,0 +1,90 @@
+// Note this object is purely in memory
+// When node shuts down this will be cleared.
+// Same when your heroku app shuts down from inactivity
+// We will be working with databases in the next few weeks.
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users: JSON.stringify(users),
+  };
+
+  console.log(users);
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const notReal = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  respondJSON(request, response, 400, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Song and artist are both required',
+  };
+
+  if (!body.song || !body.artist) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (users[body.song]) {
+    responseCode = 204;
+  } else {
+    users[body.song] = {};
+    users[body.song].song = body.song;
+  }
+
+  users[body.song].artist = body.artist;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const notRealUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Song and artist are both required',
+  };
+
+  if (!body.song || !body.artist) {
+    responseJSON.id = 'notFound';
+    return respondJSON(request, response, 404, responseJSON);
+  }
+
+  const responseCode = 404;
+
+  if (responseCode === 404) {
+    responseJSON.message = 'Resource Not Found';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+module.exports = {
+  getUsers,
+  addUser,
+  notRealUser,
+  notReal,
+};
